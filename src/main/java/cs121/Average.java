bec094695eaf88b3e4311935d95ad38855ac8142
Paul Buis <00pebuis@bsu.edu>
2022-01-11 10:45:17 -0500

    Inital commit

---
+++ src/main/java/cs121/Average.java
@@ -0,0 +1,12 @@
+package cs121;
+
+public class Average {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = 4;
+        int sum = x + y + z;
+        int average = sum / 3;
+        System.out.println("average = " + average);
+    }
+}
